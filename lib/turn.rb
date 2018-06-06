c606fa005c34bdf236d053c6f5969bef2b814236
Nicholas Malczewsky <n.malczewsky@gmail.com>
2018-06-06 17:53:42 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,9 @@
 
 require_relative '../lib/turn'
 
-
+def turn(board)
+  puts "Please enter 1-9:"
+end
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
