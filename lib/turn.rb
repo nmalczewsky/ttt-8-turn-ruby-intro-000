c9d388260410a94cca39b15066a50422b71ba225
Nicholas Malczewsky <n.malczewsky@gmail.com>
2018-06-06 17:51:55 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,7 +2,24 @@
 
 require_relative '../lib/turn'
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-puts "Welcome to Tic Tac Toe!"
-display_board(board)
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move? (board, index)
+  if index.between?(0, 8) &&  position_taken?(board, index) == false
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
