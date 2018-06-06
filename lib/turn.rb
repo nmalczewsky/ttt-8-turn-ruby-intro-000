38c24aa4af0e9885e4f70997d401d1f36a8fbeca
Nicholas Malczewsky <n.malczewsky@gmail.com>
2018-06-06 18:06:02 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,6 +10,10 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def move
+
+end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} \n-----------\n #{board[3]} | #{board[4]} | #{board[5]} \n-----------\n #{board[6]} | #{board[7]} | #{board[8]} "
 end
